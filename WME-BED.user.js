787e6353bb58b3e24bcab4e302546f6316e9c6a6
Ian "The_Cre8r" Harshbarger <TheCre8r@users.noreply.github.com>
2022-06-27 15:30:59 -0400

    Version 2022.06.27.02 - WME & TM Compat. Update

--- WME-BED.user.js
+++ WME-BED.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         WME BackEnd Data
 // @namespace    https://github.com/thecre8r/
-// @version      2022.06.27.01
+// @version      2022.06.27.02
 // @description  Shows Hidden Attributes, AdPins, and Gas Prices for Applicable Places
-// @include      https://www.waze.com/editor*
-// @include      https://www.waze.com/*/editor*
-// @include      https://beta.waze.com/editor*
-// @include      https://beta.waze.com/*/editor*
-// @include      https://support.google.com/waze/answer/7402261*
+// @match        https://www.waze.com/editor*
+// @match        https://www.waze.com/*/editor*
+// @match        https://beta.waze.com/editor*
+// @match        https://beta.waze.com/*/editor*
+// @match        https://support.google.com/waze/answer/7402261*
 // @exclude      https://www.waze.com/user/editor*
 // @icon         data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><g><path fill="rgb(120, 176, 191)" d="M176 256c44.11 0 80-35.89 80-80s-35.89-80-80-80-80 35.89-80 80 35.89 80 80 80zm352-128H304c-8.84 0-16 7.16-16 16v144H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v352c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-48h512v48c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V240c0-61.86-50.14-112-112-112z" class=""></path></g></svg>
 // @author       The_Cre8r
@@ -38,7 +38,7 @@
     const STORE_NAME = "WMEBED_Settings";
     const SCRIPT_NAME = GM_info.script.name;
     const SCRIPT_VERSION = GM_info.script.version.toString();
-    const SCRIPT_CHANGES = `WME Compatability Update`
+    const SCRIPT_CHANGES = `WME and Tampermonkey Compatibility Update`
     const UPDATE_ALERT = true;
     const USER = {name: null, rank:null};
     const SERVER = {name: null};
